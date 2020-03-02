de88e28c4176ff3e884eb7402dfdf702e828f4f0
Candykick <woo171tm@naver.com>
2020-03-02 22:07:55 +0900

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@
 //$ == <font color="#898989">
 //^ == <font color="#FFFFFF">
 //! == </font>
+//ㅅ == <font size="20pt">&nbsp;</font>
 
 var code = `
 #include <hanyang.h>;%
@@ -13,13 +14,13 @@ var code = `
 %
 string $title = ^“hanging university hard & software”;%
 public void print_title() {%
-|printf(“■~~~~~~~~~~■~~■~~~~~~~~~~~~~■~~■~~~~~~~~~~■~~~~~■■■■|~”);%
-|printf(“■~~~~~~~~~~■~~■~~~~~~~~~~~~~■~~■~~~~~~~~~~■~~■||||~~~”);%
-|printf(“■~~~~~~~~~~■~~■~~~~~~~~~~~~~■~~■~~~~~~~~~~■~~■||||~~~”);%
-|printf(“■■■■■~~■~~~~~~~~~~~~~■~~■■■■■~~~~~■■■||”);%
-|printf(“■~~~~~~~~~~■~~■~~~~~~~~~~~~~■~~■~~~~~~~~~~■~~~~~~~~~~~~~~~~■|~”);%
-|printf(“■~~~~~~~~~~■~~■~~~~~~~~~~~~~■~~■~~~~~~~~~~■~~~~~~~~~~~~~~~~■|~”);%
-|printf(“■~~~~~~~~~~■~~~~~■■■■~~~~~■~~~~~~~~~~■~~~■■■■|~~~~”);%
+|printf(“■~~~~~~~~~~ㅅ■~~~~~■~~~~~~~~~~~~~■~~~~~■~~~~~~~~~~ㅅ■~~~~~~~~■■■■|~”);%
+|printf(“■~~~~~~~~~~ㅅ■~~~~~■~~~~~~~~~~~~~■~~~~~■~~~~~~~~~~ㅅ■~~~~~■||||~~~”);%
+|printf(“■~~~~~~~~~~ㅅ■~~~~~■~~~~~~~~~~~~~■~~~~~■~~~~~~~~~~ㅅ■~~~~~■||||~~~”);%
+|printf(“■■■■■~~~~~■~~~~~~~~~~~~~■~~~~ㅅㅅ■■■■■~~~~~~~~■■■||”);%
+|printf(“■~~~~~~~~~~ㅅ■~~~~~■~~~~~~~~~~~~~■~~~~~■~~~~~~~~~~ㅅ■~~~~~~~~~~~~~~~~~~~■|~”);%
+|printf(“■~~~~~~~~~~ㅅ■~~~~~■~~~~~~~~~~~~~■~~~~~■~~~~~~~~~~ㅅ■~~~~~~~~~~~~~~~~~~~■|~”);%
+|printf(“■~~~~~~~~~~ㅅ■~~~~~~~~■■■■~~~~~~~~■~~~~~~~~~~ㅅ■~~~~~~■■■■|~~~~”);%
 }%
 %
 public void start() {%
@@ -29,12 +30,12 @@ public void start() {%
 |if(huhs) {%
 %
 |}%
-|you = cl //실력%
-|can++; //제한%
-|get.com//없음%
-|whatever //관심%
-|you = jo //있는%
-|want++; //당신%
+|you = club |~//실력%
+|can++; ||~~~//제한%
+|get.come |~~//없음%
+|whatever |~~//관심%
+|you = join |~//있는%
+|want++; ||//당신%
 }%
 %
 public void contact(string case) {%
@@ -47,6 +48,11 @@ public void contact(string case) {%
 var typingBool = false;
 var typingIndex = 0;
 var typingTxt = $(".code").text();
+
+var p = 0;
+var pct = 0;
+var prg = document.getElementById("progress");
+
 typingTxt = code.split(""); //한 글자씩 자른다.
 if(typingBool == false) {//타이핑이 진행되지 않았으면
     typingBool = true;
@@ -70,13 +76,68 @@ function typing() {
             $(".code").append('<font color="#FFFFFF">');
         } else if(typingTxt[typingIndex] == '!') {
             $(".code").append('</font>');
+        } else if(typingTxt[typingIndex] == 'ㅅ') {
+            $(".code").append('<font size="1">&nbsp;</font>'); //4.5pt
         } else {
             $(".code").append(typingTxt[typingIndex]);
         }
         typingIndex++;
     } else {
         clearInterval(tyInt); //모든 글자를 붙였으면 반복 종료.
-        $(".white_alpha").css("display","inline");
-        //$(".code").css("color","rgba(255,255,255,0.8)")
+        setTimeout(showLoading, 600);
     }
+}
+
+function showLoading() {
+    $(".white_alpha").css("display","inline");
+    animateCSS('.alert', 'fadeIn')
+    animateCSS('.progressbar', 'fadeIn', function() {
+        var loadingAni = setInterval(loading, 50);
+
+        function loading() {
+            if(prg) {
+                p++;
+                if(p <= 79)
+                {
+                    prg.style.backgroundPosition = p + "vw 0px";
+                    pct++;
+                    if(pct >= 0)
+                        {pct = -79;}
+                    prg.style.left = pct + "vw";
+                } else {
+                    clearInterval(loadingAni);
+        
+                    animateCSS('.progressbar', 'bounceOutDown', function() {
+                        $(".progressbar").css("display", "none");
+                        $(".white_alpha").css("display", "none");
+                        
+                        animateCSS('.code', 'fadeOutDown', function() {
+                            $(".code").css("display", "none");
+    
+                            $(".page_second").css("display", "inline");
+                            $(".invite").css("display", "inline");
+                            animateCSS('.invite', 'fadeInUp');
+                        });
+                    })
+                    animateCSS('.alert', 'bounceOutDown', function() {
+                        $(".alert").css("display", "none");
+                    })
+                }
+            }
+        }
+    })
+}
+
+function animateCSS(element, animationName, callback) {
+    const node = document.querySelector(element)
+    node.classList.add('animated', animationName)
+
+    function handleAnimationEnd() {
+        node.classList.remove('animated', animationName)
+        node.removeEventListener('animationend', handleAnimationEnd)
+
+        if (typeof callback === 'function') callback()
+    }
+
+    node.addEventListener('animationend', handleAnimationEnd)
 }
